9f76dec2c7e683952bdbd255fc106b4ab1dbeaab
Lejla Kasum <lkasum1@etf.unsa.ba>
2018-04-02 22:22:38 +0200

    Admin panel, dodavanje uposlenika

--- Projekat/Aerodrom/Aerodrom/App.xaml.cs
+++ Projekat/Aerodrom/Aerodrom/App.xaml.cs
@@ -66,7 +66,7 @@ namespace Aerodrom
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(RezervacijaKarataNaSalteru), e.Arguments);
+                    rootFrame.Navigate(typeof(AdministratorPanel), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
